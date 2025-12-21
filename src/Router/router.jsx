3148a3c276bc3f1783858301a8aa6642ca9f15d8
Rana Islam <dev.rana.cse@gmail.com>
2025-12-21 16:16:30 +0600

    first commit

---
+++ src/Router/router.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../Layouts/MainLayout";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: <MainLayout></MainLayout>
+  },
+]);
