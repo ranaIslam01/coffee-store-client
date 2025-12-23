9ae7a6fc69f710aea2c74d50197605f7d83c0835
Rana Islam <dev.rana.cse@gmail.com>
2025-12-23 13:48:56 +0600

    loading coffee

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -15,7 +15,7 @@ const LoadingSpinner = () => {
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-50">
       {/* Tailwind CSS Spinner */}
       <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-orange-600"></div>
-      <h2 className="mt-4 text-xl font-semibold text-gray-700">কফি শপ লোড হচ্ছে...</h2>
+      <h2 className="mt-4 text-xl font-semibold text-gray-700">Loading coffee...</h2>
     </div>
   );
 };
